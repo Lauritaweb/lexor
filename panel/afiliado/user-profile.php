4238949d46d730a9fbe08f87335ea77b1931cbd7
AlfreConde <alfredoconde@flexways.com>
2025-01-19 20:02:08 -0300

    arreglo missing }

--- panel/afiliado/user-profile.php
+++ panel/afiliado/user-profile.php
@@ -183,6 +183,7 @@ function generateTimeOptions($selectedTime = null)
                   </li>
                 <?php } ?>
               </ul>
+
               <div class="tab-content pt-2">
 
                 <div class="tab-pane fade <?php echo (Utils::isAffiliateLogged() && !$isEditing) || (Utils::isAssessorLogged() || Utils::isAdminLogged()) && $action == "view" ? "show active" : ""; ?> profile-overview" id="profile-overview">
@@ -237,10 +238,12 @@ function generateTimeOptions($selectedTime = null)
 
                   <div class="row">
                     <div class="col-lg-3 col-md-4 label">Titulo</div>
-                    <div class="col-lg-9 col-md-8"><?php
+                    <div class="col-lg-9 col-md-8">
+                      <?php
                                                     if (isset($url_file_degree))
                                                       echo "<a href='uploads/$url_file_degree' target='_blank' > $url_file_degree </a>";
-                                                    ?></div>
+                      ?>
+                      </div>
                   </div>
                 </div>
 
@@ -457,49 +460,10 @@ function generateTimeOptions($selectedTime = null)
                       </div>
                   </form><!-- End Profile Edit Form -->
 
+                  </div>
                 </div>
 
-                <div class="tab-pane fade pt-3" id="profile-settings">
-
-                  <!-- Settings Form -->
-                  <form>
-                    <div class="row mb-3">
-                      <label for="fullName" class="col-md-4 col-lg-3 col-form-label">Email Notifications</label>
-                      <div class="col-md-8 col-lg-9">
-                        <div class="form-check">
-                          <input class="form-check-input" type="checkbox" id="changesMade" checked>
-                          <label class="form-check-label" for="changesMade">
-                            Changes made to your account
-                          </label>
-                        </div>
-                        <div class="form-check">
-                          <input class="form-check-input" type="checkbox" id="newProducts" checked>
-                          <label class="form-check-label" for="newProducts">
-                            Information on new products and services
-                          </label>
-                        </div>
-                        <div class="form-check">
-                          <input class="form-check-input" type="checkbox" id="proOffers">
-                          <label class="form-check-label" for="proOffers">
-                            Marketing and promo offers
-                          </label>
-                        </div>
-                        <div class="form-check">
-                          <input class="form-check-input" type="checkbox" id="securityNotify" checked disabled>
-                          <label class="form-check-label" for="securityNotify">
-                            Security alerts
-                          </label>
-                        </div>
-                      </div>
-                    </div>
-
-                    <div class="text-center">
-                      <button type="submit" class="btn btn-primary">Save Changes</button>
-                    </div>
-                  </form><!-- End settings Form -->
-
-                </div>
-
+               
                 <!--Schedule / Horarios Pane -->
                 <div class="tab-pane fade pt-3" id="profile-schedule">
 
